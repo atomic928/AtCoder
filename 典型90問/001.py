7aac5adc4628d2602c93dc382b893f9f45b8e8ca
atomic928 <64305950+atomic928@users.noreply.github.com>
2021-05-09 16:04:16 +0900

    shouzin

--- 典型90問/001.py
+++ 典型90問/001.py
@@ -9,25 +9,8 @@ a.append(l)
 for i in range(1,n+1):
   sa[i] = a[i] - a[i-1]
 
-
-
-kosuu = k+1
-
-piecel = 1
-
-while kosuu > k:
-  kosuu = 0
-  leng = 0
-  for i in sa:
-    leng += i
-    if leng >= piecel:
-      leng = 0
-      kosuu += 1
-  piecel *= 2
-
-kosuu = k+1
-piecels = piecel//4
-piecelb = piecel//2
+piecels = 1
+piecelb = 10**9
 piecel = (piecels+piecelb)//2
 
 while piecels != piecel:
@@ -43,9 +26,5 @@ while piecels != piecel:
   else:
     piecelb = piecel
   piecel = (piecels+piecelb)//2
-
-
-
-
 
 print(piecel)
