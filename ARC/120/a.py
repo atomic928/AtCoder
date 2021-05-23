4da0fd13b5455053b4f12fa971f7740f38212b96
atomic928 <64305950+atomic928@users.noreply.github.com>
2021-05-24 00:18:27 +0900

    ARC120

--- ARC/120/a.py
+++ ARC/120/a.py
@@ -1,4 +1,5 @@
 import sys
+from itertools import accumulate
 
 #input()
 def I(): return sys.stdin.readline()
@@ -10,3 +11,18 @@ def MI(): return map(int, sys.stdin.readline().split())
 def LI(): return list(map(int, sys.stdin.readline().split()))
 #行列
 def LLI(rows_number): return [LI() for _ in range(rows_number)]
+
+n = II()
+a = LI()
+
+mx = [0]*n
+mx[0] = a[0]
+
+aacu = list(accumulate(accumulate(a)))
+
+for i in range(1, n):
+  mx[i] = max(mx[i-1], a[i])
+
+for i in range(n):
+  ans = aacu[i]+mx[i]*(i+1)
+  print(ans)
--- ARC/120/b.py
+++ ARC/120/b.py
@@ -1,12 +1,64 @@
 import sys
 
 #input()
-def I(): return sys.stdin.readline()
+def I(): return sys.stdin.readline().rstrip()
 #int(input())
-def II(): return int(sys.stdin.readline())
+def II(): return int(sys.stdin.readline().rstrip())
 #map(int,input().split())
-def MI(): return map(int, sys.stdin.readline().split())
+def MI(): return map(int, sys.stdin.readline().rstrip().split())
 #list(map(int,input().split()))
-def LI(): return list(map(int, sys.stdin.readline().split()))
+def LI(): return list(map(int, sys.stdin.readline().rstrip().split()))
 #行列
 def LLI(rows_number): return [LI() for _ in range(rows_number)]
+
+h,w = MI()
+s = [list(I()) for _ in range(h)]
+ans = 1
+
+
+if s[0][0] == ".":
+  ans *= 2
+if s[h-1][w-1] == ".":
+  ans *= 2
+
+for i in range(1, h):
+  check = ""
+  for j in range(i+1):
+    if j > w-1:
+      break
+    if s[i-j][j] == "R":
+      if check == "B":
+        print(0)
+        exit()
+      else:
+        check = "R"
+    elif s[i-j][j] == "B":
+      if check == "R":
+        print(0)
+        exit()
+      else:
+        check = "B"
+  if check == "":
+    ans *= 2
+  
+for i in range(1, w-1):
+  check = ""
+  for j in range(i+1):
+    if j > h-1:
+      break
+    if s[h-1-j][w-1-i+j] == "R":
+      if check == "B":
+        print(0)
+        exit()
+      else:
+        check = "R"
+    elif s[h-1-j][w-1-i+j] == "B":
+      if check == "R":
+        print(0)
+        exit()
+      else:
+        check = "B"
+  if check == "":
+    ans *= 2
+
+print(ans%998244353)
