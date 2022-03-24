8803b12246539f063b5ce6ae8228a33008d29179
atomic928 <64305950+atomic928@users.noreply.github.com>
2022-03-24 20:02:15 +0900

    典型90

--- 典型90問/02.py
+++ 典型90問/02.py
@@ -16,3 +16,34 @@ def LI(): return list(MI())
 def LLI(rows_number): return [LI() for _ in range(rows_number)]
 #文字の行列
 def LSI(rows_number): return [SI() for _ in range(rows_number)]
+
+n = II()
+ls = []
+
+# 奇数の場合は正しくない
+if n%2 == 1:
+  exit()
+
+for i in range(2**n):
+  s = ""
+  left = 0
+  right = 0
+  for j in range(n):
+    if i >> j & 1:
+      left += 1
+      s += "("
+    else:
+      right += 1
+      s += ")"
+    # 正しくなくなったらbreak
+    if left < right:
+      break
+  else:
+    # 左と右のカッコの数が等しいときだけ追加
+    if left == right:
+      ls.append(s)
+    
+# 辞書順に並べ、出力
+ls.sort()
+for i in ls:
+  print(i)
